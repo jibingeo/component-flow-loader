43f060d531d0d511bdeb1cbcea462f62fae89a07
Gurdas Nijor <nijor22@gmail.com>
2014-12-01 00:29:23 -0800

    Added method for parsing propTypes

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ var falafel = require('falafel');
 var _  = require('lodash');
 
 
+/**
+ *
+ * @param source - Component sourcecode
+ * @returns {Array}
+ *  [{ name: 'className', type: 'ReactPropTypes.string' },
+ *   { name: 'id', type: 'ReactPropTypes.string' },
+ *   { name: 'placeholder', type: 'ReactPropTypes.string' },
+ *   { name: 'onSave', type: 'ReactPropTypes.func.isRequired' },
+ *   { name: 'value', type: 'ReactPropTypes.string' } ]
+ */
+
+function parseProps(source){
+
+  var parsedPropsArray
+
+  falafel(source, function (node) {
+    if (node.type === 'Property' && node.key.name === 'propTypes') {
+
+      parsedPropsArray = _(node.parent.properties).filter(function (p) {
+        return p.type == "Property" && p.key.name == "propTypes"
+      }).map(function (n) {
+       // console.log(self.request);
+        return _.map(n.value.properties, function (e) {
+          return {name: e.key.name, type: e.value.source()}
+        });
+      }).flatten().value();
+    }
+  });
+
+  return parsedPropsArray;
+}
+
+
+
 module.exports = function(content) {
 
   var self = this;
